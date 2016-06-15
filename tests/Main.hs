8ebd1dd058670bc455d8aeb0e84197a9bcb8f007
Alex Sayers <alex.sayers@gmail.com>
2016-06-15 10:19:42 +0900

    Generalise tests

--- tests/Main.hs
+++ tests/Main.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE Rank2Types #-}
 
 module Main where
 
@@ -14,35 +15,41 @@ import Test.Tasty
 import Test.Tasty.Golden
 
 import Database.Kafkalite
+import Database.Kafkalite.Backend.FileSystem
 
 main :: IO ()
-main = defaultMain =<< testFileSystemBackend
+main = defaultMain =<< testGroup "kafkalite" <$> sequence
+    [ testBackend "filesystem backend" runFileSystemM'
+    ]
 
+testDataDir :: FilePath
 testDataDir = "tests/test-data"
+
+runFileSystemM' :: FileSystemM a -> IO a
+runFileSystemM' x = runFileSystemM x (testDataDir ++ "/fs-data")
 
 -------------------------------------------------------------------------------
 -- Testing the filesystem backend
 
-testFileSystemBackend :: IO TestTree
-testFileSystemBackend =
-    testGroup "filesystem backend" . map testTopic <$>
-        listDirectory (testDataDir ++ "/fs-golden/")
+testBackend :: (MonadKafka m, MonadThrow m) => String -> (forall a. m a -> IO a) -> IO TestTree
+testBackend backendName runMonadKafka =
+    testGroup backendName . map (testTopic runMonadKafka) <$>
+        listDirectory (testDataDir ++ "/golden/")
 
-testTopic :: String -> TestTree
-testTopic testName =
+testTopic :: (MonadKafka m, MonadThrow m) => (forall a. m a -> IO a) -> String -> TestTree
+testTopic runMonadKafka testName =
     let (topicName, partition) = parseTopicPartition testName
     in goldenVsString
         ("topic " ++ testName)
-        (testDataDir ++ "/fs-golden/" ++ testName)
-        (exhaustTopic topicName partition)
+        (testDataDir ++ "/golden/" ++ testName)
+        (runMonadKafka $ exhaustTopic topicName partition)
 
 parseTopicPartition :: String -> (String, Int)
 parseTopicPartition xs = let (ls, rs) = span (/= '-') (reverse xs)
     in (reverse (drop 1 rs), read (reverse ls))
 
-exhaustTopic :: String -> Int -> IO BL.ByteString
+exhaustTopic :: (MonadKafka m, MonadThrow m) => String -> Int -> m BL.ByteString
 exhaustTopic topicName partition = do
-    let baseDir = testDataDir ++ "/fs-test"
-    topic <- loadTopic baseDir topicName partition
-    fmap TL.encodeUtf8 $ runSafeT $ PT.toLazyM $
+    topic <- loadTopic topicName partition
+    fmap TL.encodeUtf8 $ PT.toLazyM $
         readTopic topic (Offset 0) >-> P.map (T.pack . (++ "\n") . ppMessage)
